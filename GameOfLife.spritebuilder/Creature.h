e33b8c98bb6252a3eebab790e1b867189ea414c6
Esteban Piazza Vázquez <Esteban@MacBook-Pro-de-Esteban.local>
2014-06-24 13:11:04 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Creature.h
+++ GameOfLife.spritebuilder/Creature.h
@@ -9,10 +9,13 @@
 #import "CCSprite.h"
 
 @interface Creature : CCSprite
-@property (nonatomic,assign) BOOL isAlive;
 
+// stores the current state of the creature
+@property (nonatomic, assign) BOOL isAlive;
+
+// stores the amount of living neighbors
 @property (nonatomic, assign) NSInteger livingNeighbors;
 
--(id)initCreature;
+- (id)initCreature;
 
 @end
