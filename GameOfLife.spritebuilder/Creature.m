94f905708762f89b4598259d9c63081319832082
Esteban Piazza Vázquez <Esteban@MacBook-Pro-de-Esteban.local>
2014-06-24 13:01:07 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Creature.m
@@ -0,0 +1,28 @@
+//
+//  Creature.m
+//  GameOfLife
+//
+//  Created by Esteban Piazza Vázquez on 24/06/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Creature.h"
+
+@implementation Creature
+
+-(instancetype)initCreature{
+    self = [super initWithImageNamed:@"GameOfLifeAssets/Assets/bubble.png"];
+    
+    if(self){
+        self.isAlive = NO;
+    }
+    return self;
+}
+
+-(void) setIsAlive:(BOOL)newState {
+    _isAlive = newState;
+    
+    self.visible = _isAlive;
+}
+
+@end
